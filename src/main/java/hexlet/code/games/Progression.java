d253c70a095d8629aaabfe90b5159b95ebf4e35d
m.balashov <m.balashov@crpt.ru>
2021-11-29 17:03:24 +0300

    ch progression game

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -5,26 +5,31 @@ import hexlet.code.Engine;
 public class Progression {
 
     public static void progression() {
-        Engine.gameGreeting();
-        System.out.println("What number is missing in the progression?");
+        String rules = "What number is missing in the progression?";
+        Engine.fullGame(rules, generateValues());
+    }
 
+    public static String[][] generateValues() {
+        String[][] qna = new String[Engine.getPlayTimes()][2];
         final int maxStartValue = 100;
         final int maxStep = 20;
         final int maxPlusSize = 10;
         for (int i = 0; i < Engine.getPlayTimes(); i++) {
-            int rowLength = maxPlusSize / 2 + Engine.randomize(maxPlusSize);
-            int step = 1 - maxStep / 2 + Engine.randomize(maxStep);
-            int startValue = -maxStartValue / 2 + Engine.randomize(maxStartValue);
+            int rowLength = valueLength(maxPlusSize);
+            int step = 1 + value(maxStep);
+            int startValue = value(maxStartValue);
             int randomNumber = Engine.randomize(rowLength);
-            String[] qna = questionAnswer(rowLength, startValue, step, randomNumber);
-            String question = qna[0].substring(0, qna[0].length() - 1);
-            String correctAnswer = qna[1];
-            String answer = Engine.question(question);
-            if (Engine.isCorrect(correctAnswer, answer)) {
-                return;
-            }
+            qna[i] = questionAnswer(rowLength, startValue, step, randomNumber);
         }
-        Engine.grats();
+        return qna;
+    }
+
+    public static int valueLength(int max) {
+        return (max/2 + Engine.randomize(max));
+    }
+
+    public static int value(int max) {
+        return (-max/2 + Engine.randomize(max));
     }
 
     public static String[] questionAnswer(int rowLength, int startValue, int step, int randomNumber) {
@@ -39,6 +44,7 @@ public class Progression {
                 qna[0] = qna[0] + startValue + " ";
             }
         }
+        qna[0] = qna[0].substring(0, qna[0].length() - 1);
         return qna;
     }
 }
